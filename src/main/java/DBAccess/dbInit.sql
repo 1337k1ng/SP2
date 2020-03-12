6d88cf5d0f120651c5f6060d6125226f01648a7b
BenjaminIglesias <cph-bc106@cphbusiness.dk>
2020-03-13 00:01:11 +0100

    DatabaseInit(vores script sat ind) + Url og login til tobias DB

--- src/main/java/DBAccess/dbInit.sql
+++ src/main/java/DBAccess/dbInit.sql
@@ -2,25 +2,76 @@
 
 -- noinspection SqlNoDataSourceInspectionForFile
 
-CREATE DATABASE  IF NOT EXISTS `useradmin`;
+CREATE SCHEMA IF NOT EXISTS CupcakeDB;
 
 
-USE `useradmin`;
+USE CupcakeDB;
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(90) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  `role` varchar(20) NOT NULL DEFAULT 'customer',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
-LOCK TABLES `users` WRITE;
-INSERT INTO `users` VALUES 
-(1,'jens@somewhere.com','jensen','customer'),
-(2,'ken@somewhere.com','kensen','customer'),
-(3,'robin@somewhere.com','batman','employee');
-UNLOCK TABLES;
+CREATE TABLE Bruger (
+  id int AUTO_INCREMENT,
+  navn varchar(255),
+  email varchar(255),
+  password varchar(255),
+  saldo int,
+  rolle varchar(255),
+  PRIMARY KEY (id)
 
+);
+
+
+CREATE TABLE Ordre (
+  ordreId int AUTO_INCREMENT,
+  id int,
+  PRIMARY KEY (ordreId),
+    FOREIGN KEY (id) REFERENCES Bruger(id)
+);
+
+
+CREATE TABLE Indhold (
+  ordreId int,
+  cupcakeId int,
+  antal int,
+  PRIMARY KEY (ordreId, cupcakeId),
+  FOREIGN KEY (ordreId) REFERENCES Ordre(ordreId)
+
+);
+
+
+CREATE TABLE Tidspunkt (
+  ordreId int,
+  Dato varchar(255),
+  PRIMARY KEY (ordreId),
+    FOREIGN KEY (ordreId) REFERENCES Ordre(ordreId)
+);
+
+
+
+CREATE TABLE Bund (
+  bundId int auto_increment not null,
+  navn varchar(255),
+  pris int,
+  PRIMARY KEY (bundId)
+);
+
+
+CREATE TABLE Top (
+  topId int auto_increment not null,
+  navn varchar(255),
+  pris int,
+  PRIMARY KEY (topId)
+);
+
+
+CREATE TABLE Cupcake (
+  cupcakeId int auto_increment not null,
+  bundId int,
+  topId int,
+  PRIMARY KEY (cupcakeId),
+  FOREIGN KEY (bundId) REFERENCES Bund(bundId),
+  FOREIGN KEY (topId) REFERENCES Top(topId)
+
+);
+
+ALTER TABLE Indhold
+ADD FOREIGN KEY (cupcakeId) REFERENCES Cupcake(cupcakeId);
