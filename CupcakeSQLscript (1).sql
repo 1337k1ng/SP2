117ca5e728dba8a5e57093d3b76453c320e37d6b
BenjaminIglesias <56401369+BenjaminIglesias@users.noreply.github.com>
2020-03-12 16:08:53 +0100

    Add files via upload
    Database script

---
+++ CupcakeSQLscript (1).sql
@@ -0,0 +1,77 @@
+-- noinspection SqlDialectInspectionForFile
+
+-- noinspection SqlNoDataSourceInspectionForFile
+
+CREATE SCHEMA IF NOT EXISTS CupcakeDB;
+
+
+USE CupcakeDB;
+
+
+CREATE TABLE Bruger (
+  id int AUTO_INCREMENT,
+  navn varchar(255),
+  email varchar(255),
+  password varchar(255),
+  saldo int, 
+  rolle varchar(255),
+  PRIMARY KEY (id)
+
+);
+
+
+CREATE TABLE Ordre (
+  ordreId int AUTO_INCREMENT,
+  id int,
+  PRIMARY KEY (ordreId),
+    FOREIGN KEY (id) REFERENCES Bruger(id)
+);
+
+
+CREATE TABLE Indhold (
+  ordreId int,
+  cupcakeId int,
+  antal int,
+  PRIMARY KEY (ordreId, cupcakeId),
+  FOREIGN KEY (ordreId) REFERENCES Ordre(ordreId)
+
+);
+
+
+CREATE TABLE Tidspunkt (
+  ordreId int,
+  Dato varchar(255),
+  PRIMARY KEY (ordreId),
+    FOREIGN KEY (ordreId) REFERENCES Ordre(ordreId)
+);
+
+
+
+CREATE TABLE Bund (
+  bundId int auto_increment not null,
+  navn varchar(255),
+  pris int, 
+  PRIMARY KEY (bundId)
+);
+
+
+CREATE TABLE Top (
+  topId int auto_increment not null,
+  navn varchar(255),
+  pris int, 
+  PRIMARY KEY (topId)
+);
+
+
+CREATE TABLE Cupcake (
+  cupcakeId int auto_increment not null,
+  bundId int,
+  topId int, 
+  PRIMARY KEY (cupcakeId),
+  FOREIGN KEY (bundId) REFERENCES Bund(bundId),
+  FOREIGN KEY (topId) REFERENCES Top(topId)
+
+);
+
+ALTER TABLE Indhold
+ADD FOREIGN KEY (cupcakeId) REFERENCES Cupcake(cupcakeId);
